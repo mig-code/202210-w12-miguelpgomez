5b7ddb7f4ff7898cbfe5387c7a86d970b8022122
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-15 14:13:27 +0100

    :building_construction: Add basic project structur

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import App from './core/components/app/App';
 import reportWebVitals from './reportWebVitals';
+import './css/styles.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
