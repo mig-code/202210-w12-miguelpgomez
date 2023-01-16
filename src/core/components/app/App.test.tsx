8df36cbb87b4086fcc19471c66075713f874e2c2
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-16 09:57:31 +0100

    :pencil2: Fix typo on default test

--- src/core/components/app/App.test.tsx
+++ src/core/components/app/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
     render(<App />);
     const heading = screen.getByText(/Lista de personajes/i);
     expect(heading).toBeInTheDocument();
