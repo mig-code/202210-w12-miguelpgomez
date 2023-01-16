29372684a24acbfc7ff6a7ae55da0c5dd7c1a6e9
Miguel <99726342+mig-code@users.noreply.github.com>
2023-01-16 13:41:07 +0100

    :test_tube: Add basic UseCharacter Test

---
+++ src/core/components/hooks/use.character.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+
+import { useCharacters } from './use.characters';
+import { CharacterOption } from '../../data/characters.data';
+import { mockAdvisor } from '../../mocks/models.mocks';
+
+describe(`Given useCharacter (custom hook)
+            render with a virtual component`, () => {
+    const mockPayload: Partial<CharacterOption> = {
+        id: '1',
+    };
+
+    const TestComponent = () => {
+        const { getCharacters, handleUpdate } = useCharacters();
+        return (
+            <>
+                <button onClick={() => handleUpdate(mockPayload)}>
+                    Update
+                </button>
+
+                <div>
+                    <ul>
+                        {getCharacters().map((item) => (
+                            <li key={item.id}>{item.char_name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </>
+        );
+    };
+
+    describe(`When component is loaded`, () => {
+        render(<TestComponent />);
+        const buttons = screen.getAllByRole('button');
+        test('Then its should call handleUpdate', async () => {
+            expect(
+                await screen.findByText(mockAdvisor.char_name)
+            ).toBeInTheDocument();
+
+           fireEvent.click(buttons[0]);
+        });
+    });
+});
